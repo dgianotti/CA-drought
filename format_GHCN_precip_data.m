f6ca2f468fbbbbeb1e14f89570af1012bf4274e0
dgianotti <gianotti@bu.edu>
2014-03-12 15:32:40 -0400

    Creates function format_GHCN_precip_data. Adds to CompileStnData.

--- format_GHCN_precip_data.m
+++ format_GHCN_precip_data.m
@@ -1,19 +1,8 @@
-% Let's get the (current) data for the CA stations!
-
-clear;
-clc;
-
-load('CA_ids.mat');
-
-%% Download data:
-% for i = 1:length(CA_IDs)
-id = CA_IDs{1}; % Berkeley
-% Download the latest USHCN data:
-url = ['http://www1.ncdc.noaa.gov/pub/data/ghcn/daily/hcn/USC00',id,'.dly'];
-filename = [id,'.dly'];
-urlwrite(url,filename);
-
-%% Read in fixed-width data:
+function [datenums, precip] = format_GHCN_precip_data(filename)
+% The function format_GHCN_precip_data takes as its input a single file
+% of daily data from the GHCN/USHCN (usually .dly) and outputs two vectors:
+%
+% datenums is an N x 1 vector of date-numbers (see 'help datenum')
 fid = fopen(filename);
 % ------------------------------
 % Variable   Columns   Type
@@ -98,35 +87,15 @@ precip_vec(precip_vec < 0) = nan;
 
 precip_vec = precip_vec/10; % Now in mm
 
+unfilled = (month_vec == 0);
+year_vec(unfilled) = [];
+month_vec(unfilled) = [];
+day_vec(unfilled) = [];
+precip_vec(unfilled) = [];
+
+
 date_vec = datenum( [num2str(year_vec),repmat('-',[length(precip_vec),1]),...
     num2str(month_vec,'%02d'),repmat('-',[length(precip_vec),1]),num2str(day_vec,'%02d')] );
 
 
-%%
-
-A = importdata('301137.csv',',');
-dates = A.data(:,1);
-precip = A.data(:,2)/10; % in mm
-
-% throw out the data before 2010:
-old_data = dates<20100000;
-dates(old_data) = [];
-precip(old_data) = [];
-
-%% Convert dates to date numbers:
-% Convert dates to strings:
-dates = num2str(dates);
-n = size(dates,1);
-
-% First 4 digits are yyyy
-yyyy = dates(:,1:4);
-mm = dates(:,5:6);
-dd = dates(:,7:8);
-date_str = horzcat(yyyy,repmat('-',[n,1]),mm,repmat('-',[n,1]),dd); % now yyyy-mm-dd
-date_nums = datenum(date_str);
-
-%%
-%plot(precip)
-plot(date_nums, precip)
-datetick('x');
-% Well... that doesn't look good...
+end
